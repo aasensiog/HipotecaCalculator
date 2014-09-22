28c1c22128ae44fe5cca0d971f4880a782fa383e
Albert Asensio Gimenez <aasensiog@ymail.com>
2014-09-22 22:07:35 +0200

    stilo y validaciones

--- js/app.js
+++ js/app.js
@@ -5,14 +5,22 @@ $(document).on('pageshow', '#index', function() {
         	interes_anual = parseFloat($('input#interes').val()),
         	anos = parseFloat($('input#años').val());
 
-        var tasa_interes_mensual = interes_anual / 1200;
-        var dinero_interes_mensual = dinero * tasa_interes_mensual;
-        var tasa_interes_mensual_mas_uno = tasa_interes_mensual + 1;
-        var potencia = Math.pow(tasa_interes_mensual_mas_uno, -(anos * 12));
-        var resta_uno = 1 - potencia;
-        var mensual = dinero_interes_mensual / resta_uno;
+        if ($.isNumeric(dinero) && $.isNumeric(interes_anual) && $.isNumeric(anos)) {
+            var tasa_interes_mensual = interes_anual / 1200,
+                dinero_interes_mensual = dinero * tasa_interes_mensual,
+                tasa_interes_mensual_mas_uno = tasa_interes_mensual + 1,
+                potencia = Math.pow(tasa_interes_mensual_mas_uno, - (anos * 12)),
+                resta_uno = 1 - potencia,
+                mensual = dinero_interes_mensual / resta_uno;
 
-        mostrar_resultado(mensual);
+            mostrar_resultado((mensual.toFixed(2)).toString() + '€');
+        } else {
+            $.mobile.showPageLoadingMsg("c", "Los datos deben ser numéricos.");
+            mostrar_resultado('');
+            setTimeout(function() {
+                $.mobile.hidePageLoadingMsg();
+            }, 1500);
+        }
     });
 });
 
